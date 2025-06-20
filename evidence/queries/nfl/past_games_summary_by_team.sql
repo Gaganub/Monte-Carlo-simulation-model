4968fa9ee50657bdd4fc769ba18006ec815414be
Gagan U Biradar <138388119+Gaganub@users.noreply.github.com>
2025-06-20 11:29:11 +0530

    past_games_summary_by_team.sql

--- evidence/queries/nfl/past_games_summary_by_team.sql
+++ evidence/queries/nfl/past_games_summary_by_team.sql
@@ -2,7 +2,7 @@ WITH cte_team AS
     (SELECT winning_team AS team FROM ${past_games} GROUP BY ALL)
 SELECT
     T.Team,
-    COUNT(*) as total_games_played, 
+    COUNT(*) as total_games_played,
     SUM(PG.accurate_guess) AS correct_predictions,
     correct_predictions/total_games_played::real AS accuracy_pct1
 FROM ${past_games} PG
